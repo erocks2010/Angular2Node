48ebe6d9d5de2bf4b717c11845f7d0b52dce5ffc
erocks2010 <tarun.mathur@hotmail.com>
2017-11-13 01:12:35 +0530

    records pulling up.But need to user more RX to make it better structured

--- client/src/app/navigation/nav-bar-main/nav-bar-main.component.ts
+++ client/src/app/navigation/nav-bar-main/nav-bar-main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FetchMongodbDataService } from '../../services/fetch-mongodb-data.service';
 import { NavBarMenuItem } from '../../custom-types/nav-bar-menu-item';
+import { Observable } from 'rxjs/observable';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'nav-bar-main',
@@ -11,10 +13,23 @@ import { NavBarMenuItem } from '../../custom-types/nav-bar-menu-item';
 })
 export class NavBarMainComponent implements OnInit {
   navBarItems: Array<NavBarMenuItem>;
+  notification$: Observable<Array<NavBarMenuItem>>;
   constructor(private mongoDb: FetchMongodbDataService) { }
 
   ngOnInit() {
-    this.navBarItems = this.mongoDb.getNavBarAll();
+    // this.notification$ = this.mongoDb.notification$;
+    // this.notification$
+    //   .subscribe((data) => {
+    //     this.navBarItems = data;
+    //   })
+    this.mongoDb.getNavBarAll()
+      .subscribe((data) => {
+        this.navBarItems = data;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) console.log('Unhandled Error ', err.error.message);
+        else console.log('Node Server returned status code ', err.status, err.error);
+      });;
   }
 
 
--- client/src/app/services/fetch-mongodb-data.service.ts
+++ client/src/app/services/fetch-mongodb-data.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { NavBarMenuItem } from '../custom-types/nav-bar-menu-item';
-import 'rxjs/add/operator/retry';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/oberator/return'
 import { Observable } from 'rxjs/observable';
+import { Observer } from 'rxjs/observer';
+import 'rxjs/Rx';
 
 @Injectable()
 export class FetchMongodbDataService {
+  notification$: Observable<Array<NavBarMenuItem>>;
+  observer: Observer<Array<NavBarMenuItem>>;
+  constructor(private http: HttpClient) {
+    // this.notification$ = new Observable(observer => this.observer = observer).share();
+  }
 
-  constructor(private http: HttpClient) { }
-
-  getNavBarAll(): Array<NavBarMenuItem> {
-    let _data=Observable.of(null);
-    this.http.get<Array<NavBarMenuItem>>('http://localhost:3000/api/navBarMenuItems')
-    .retry(3)
-      .subscribe((data) => {
-        _data.next;
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) console.log('Unhandled Error ', err.error.message);
-        else console.log('Node Server returned status code ', err.status, err.error);
-      });
-    return _data;
+  getNavBarAll() {
+    return this.http.get<Array<NavBarMenuItem>>('http://localhost:3000/api/navBarMenuItems')
+      .retry(3);
+      // .subscribe((data) => {
+      //   this.observer.next(data);
+      // },
+      // (err: HttpErrorResponse) => {
+      //   if (err.error instanceof Error) console.log('Unhandled Error ', err.error.message);
+      //   else console.log('Node Server returned status code ', err.status, err.error);
+      // });
   }
 
 }
